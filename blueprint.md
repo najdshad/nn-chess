6b6b8491fd9ee27385c211ea0eee845b63da3e7b
najdshad <alirezanajd28@gmail.com>
2025-02-18 13:07:06 +0330

    byte data structure for pieces

--- blueprint.md
+++ blueprint.md
@@ -1,1 +1,11 @@
 # Board Representation
+
+bitboard for each piece
+INIT_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+
+# Piece bit allocation
+
+7: occupied flag -> 1 = exists, 0 = empty
+6: color -> 0 = white, 1 = black
+5-3 : piece type -> 000 = Pawn, 001 = Knight, 010 = Bishop, 011 = Rook, 100 = Queen, 101 = King
+2-0 : flags -> Can be used for en passant, castling rights, promotions
--- nnchess.py
+++ nnchess.py
@@ -1,3 +1,46 @@
 import numpy as np
 
+class Piece:
+    def __init__(self, piece: np.byte):
+        self.piece = piece
+
+    def name(self):
+        type = (self.piece >> 3) & 0b111  # Extract bits 5-3
+        color = (self.piece >> 5) & 1 # Extract bit 6
+        name: str = ''
+
+        match color:
+            case 0b0:
+                name = 'White '
+            case 0b1:
+                name = 'Black '
+
+        match type:
+            case 0b000:
+                name += 'Pawn'
+            case 0b001:
+                name += 'Knight'
+            case 0b010:
+                name += 'Bishop'
+            case 0b011:
+                name += 'Rook'
+            case 0b100:
+                name += 'Queen'
+            case 0b101:
+                name += 'King'
+
+        return name
+
+x = Piece(0x88)
+
+print(x.name())
+
+
+# def fen_to_board(fen):
+#     for row in fen.split('/'):
+
+
 bitboard = np.array(np.zeros((8, 8)), dtype=bool)
+INIT_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+fen = INIT_FEN
+
